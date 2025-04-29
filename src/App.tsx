6738b6b908ed8c348a5859858a89af2656accc5e
Haruki140 <iharuki140@icloud.com>
2025-04-29 23:07:11 +0900

    メインページのレスポンシブ対応完了

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,12 @@ const Page:FC = () => {
 
       {/* 自己紹介 */}
       <div className='bg-[var(--body-color)]'>
-        <div className='h-auto max-w-360 mx-auto text-center'>
-          <h1 className='text-[64px] pt-76'>About me</h1>
-          <div className='flex items-center flex-wrap justify-center gap-10 mx-10'>
+        <div className='h-auto max-w-360 mx-auto text-center py-70'>
+          <h1 className='md:text-[64px] sm:text-[52px] text-[44px] relative'>
+            About me
+            <span className='absolute bg-[var(--text-color)] md:w-95 sm:w-80 w-65 sm:h-[5px] h-[4px] bottom-0 left-1/2 transform -translate-x-1/2' />
+          </h1>
+          <div className='flex items-center flex-wrap justify-center gap-10 mt-10'>
             <div className='lg:h-50 md:h-40 h-35 lg:w-50 md:w-40 w-35 rounded-full bg-amber-500'></div>
             <p className='lg:w-175 md:w-120 w-115 lg:text-3xl md:text-2xl text-xl lg:pl-15 inline-block text-left'>
               大阪公立大学大学院 情報学研究科 基幹情報学専攻の石丸です．
@@ -30,7 +33,7 @@ const Page:FC = () => {
               Webアプリをチームで開発したり個人でも開発しています
             </p>
           </div>
-          <div className='flex justify-center flex-wrap sm:gap-18 gap-10 mt-10'>
+          <div className='flex justify-center flex-wrap md:gap-18 gap-10 mt-10'>
             {/* 音楽 */}
             <div className='flex flex-col items-start'>
               <h1 className='text-[40px]'>
@@ -75,136 +78,138 @@ const Page:FC = () => {
       </div>
 
       {/* 活動紹介 */}
-      {/* h-345 */}
-      <div className='h-auto max-w-360 mx-auto'>
-        <h1 className='text-center pt-59 text-[64px]'>Works</h1>
-        <div className='flex flex-wrap justify-center gap-14'>
+      <div className='h-auto max-w-360 mx-auto py-70'>
+        <h1 className='text-center md:text-[64px] sm:text-[52px] text-[44px] relative'>
+          Works
+          <span className='absolute bg-[var(--text-color)] md:w-70 sm:w-60 w-50 sm:h-[5px] h-[4px] bottom-0 left-1/2 transform -translate-x-1/2' />
+        </h1>
+        <div className='flex flex-wrap justify-center lg:gap-14 gap-10 sm:mt-15 mt-5'>
           <div className='flex flex-col items-center'>
-            <div className='h-56 w-100 bg-[#D4C9BE]'></div>
-            <div className='h-34 w-100 text-center bg-[#D9D9D9]'>
+            <div className='sm:h-56 h-50 sm:w-100 w-90 bg-[#D4C9BE]'></div>
+            <div className='sm:h-34 h-30 sm:w-100 w-90 text-center bg-[#D9D9D9]'>
               <h2 className='text-[24px]'>ポートフォリオサイト</h2>
               <p className='pt-6 text-[20px]'>自分のサイトを作りました</p>
             </div>
           </div>
           <div className='flex flex-col items-center'>
-            <div className='h-56 w-100 bg-[#D4C9BE]'></div>
-            <div className='h-34 w-100 text-center bg-[#D9D9D9]'>
-              <h2 className='text-[24px]'>研究会</h2>
-              <p className='pt-6 text-[20px]'>研究会に参加しました</p>
+            <div className='sm:h-56 h-50 sm:w-100 w-90 bg-[#D4C9BE]'></div>
+            <div className='sm:h-34 h-30 sm:w-100 w-90 text-center bg-[#D9D9D9]'>
+              <h2 className='text-[24px]'>ポートフォリオサイト</h2>
+              <p className='pt-6 text-[20px]'>自分のサイトを作りました</p>
             </div>
           </div>
           <div className='flex flex-col items-center'>
-            <div className='h-56 w-100 bg-[#D4C9BE]'></div>
-            <div className='h-34 w-100 text-center bg-[#D9D9D9]'>
-              <h2 className='text-[24px]'>研究会</h2>
-              <p className='pt-6 text-[20px]'>研究会に参加しました</p>
+            <div className='sm:h-56 h-50 sm:w-100 w-90 bg-[#D4C9BE]'></div>
+            <div className='sm:h-34 h-30 sm:w-100 w-90 text-center bg-[#D9D9D9]'>
+              <h2 className='text-[24px]'>ポートフォリオサイト</h2>
+              <p className='pt-6 text-[20px]'>自分のサイトを作りました</p>
             </div>
           </div>
-          <div className='flex flex-col items-center'>
-            <div className='h-56 w-100 bg-[#D4C9BE]'></div>
-            <div className='h-34 w-100 text-center bg-[#D9D9D9]'>
-              <h2 className='text-[24px]'>研究会</h2>
-              <p className='pt-6 text-[20px]'>研究会に参加しました</p>
-            </div>
-          </div>
-          <div className='flex flex-col items-center'>
-            <div className='h-56 w-100 bg-[#D4C9BE]'></div>
-            <div className='h-34 w-100 text-center bg-[#D9D9D9]'>
-              <h2 className='text-[24px]'>研究会</h2>
-              <p className='pt-6 text-[20px]'>研究会に参加しました</p>
-            </div>
-          </div>
+          
         </div>
       </div>
 
       {/* スキル紹介 */}
       <div className='bg-[var(--body-color)]'>
         {/* h-416 */}
-        <div className='h-auto max-w-360 mx-auto'>
-          <h1 className='text-[64px] text-center pt-72'>
+        <div className='h-auto max-w-360 mx-auto py-70'>
+          <h1 className='md:text-[64px] sm:text-[52px] text-[44px] text-center relative'>
             Skills
+            <span className='absolute bg-[var(--text-color)] md:w-70 sm:w-60 w-50 sm:h-[5px] h-[4px] bottom-0 left-1/2 transform -translate-x-1/2' />
           </h1>
-          <div className='flex flex-col mx-70'>
-            <h2 className='text-[36px] text-start'>
+          <div className='flex flex-col 2xl:mx-70 xl:mx-60 lg:mx-50 md:mx-30 sm:mx-20 mx-5 sm:mt-10 mt-5'>
+            <h2 className='md:text-4xl text-3xl text-start'>
               FrontEnd
             </h2>
-            <div className='flex flex-wrap gap-7.5'>
-              <img className='h-20 w-20' src='/icons/HTML.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/CSS.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/JavaScript.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/TypeScript.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/React.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/VueJS.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Vite.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/TailwindCSS.svg' alt='1' />
+            <div className='flex flex-wrap sm:gap-7 gap-3 mt-3'>
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/HTML.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/CSS.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/JavaScript.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/TypeScript.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/React.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/VueJS.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Vite.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/TailwindCSS.svg' alt='1' />
             </div>
-            <div className='text-[32px] pt-7.5'>
-              <li>HTML：Webアプリ開発</li>
-              <li>CSS：Webアプリ開発</li>
-              <li>JavaScript：Webアプリ開発</li>
-            </div>
-          </div>
-          <div className='flex flex-col mx-70 pt-5'>
-            <h2 className='text-[36px]'>BackEnd</h2>
-            <div className='flex flex-wrap gap-7.5'>
-              <img className='h-20 w-20' src='/icons/Java.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Python.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/C.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/CPP.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Django.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Spring.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Gradle.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Mariadb.svg' alt='1' />
-            </div>
-            <div className='text-[32px] pt-7.5'>
-              <li>Python：Webアプリ開発</li>
-              <li>Java：Webアプリ開発</li>
-              <li>MySQL：Webアプリ開発</li>
+            <div className='flex flex-col tiny:gap-2 md:text-3xl tiny:text-2xl text-xl pt-7.5'>
+              <p>HTML：Webアプリ開発</p>
+              <p>CSS：Webアプリ開発</p>
+              <p>JavaScript：Webアプリ開発</p>
             </div>
           </div>
-          <div className='flex flex-col mx-70 pt-5'>
-            <h2 className='text-[36px]'>Tools</h2>
-            <div className='flex flex-wrap gap-7.5'>
-              <img className='h-20 w-20' src='/icons/VSCode.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Github.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Docker.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Figma.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Linux.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Eclipse.svg' alt='1' />
-              <img className='h-20 w-20' src='/icons/Vercel.svg' alt='1' />
+          <div className='flex flex-col 2xl:mx-70 xl:mx-60 lg:mx-50 md:mx-30 sm:mx-20 mx-5 pt-5'>
+            <h2 className='md:text-4xl text-3xl text-start'>
+              BackEnd
+            </h2>
+            <div className='flex flex-wrap sm:gap-7 gap-3 mt-3'>
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Java.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Python.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/C.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/CPP.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Django.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Spring.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Gradle.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Mariadb.svg' alt='1' />
             </div>
-            <div className='text-[32px] pt-7.5'>
-              <li>Docker：Webアプリ開発</li>
-              <li>GitHub：Webアプリ開発</li>
-              <li>VSCode：Webアプリ開発</li>
+            <div className='flex flex-col tiny:gap-2 md:text-3xl tiny:text-2xl text-xl pt-7.5'>
+              <p>Python：Webアプリ開発</p>
+              <p>Java：Webアプリ開発</p>
+              <p>MySQL：Webアプリ開発</p>
+            </div>
+          </div>
+          <div className='flex flex-col 2xl:mx-70 xl:mx-60 lg:mx-50 md:mx-30 sm:mx-20 mx-5 pt-5'>
+            <h2 className='md:text-4xl text-3xl text-start'>
+              Tools
+            </h2>
+            <div className='flex flex-wrap sm:gap-7 gap-3 mt-3'>
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/VSCode.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Github.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Docker.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Figma.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Linux.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Eclipse.svg' alt='1' />
+              <img className='sm:h-20 h-15 sm:w-20 w-15' src='/icons/Vercel.svg' alt='1' />
+            </div>
+            <div className='flex flex-col tiny:gap-2 md:text-3xl tiny:text-2xl text-xl pt-7.5'>
+              <p>Docker：Webアプリ開発</p>
+              <p>GitHub：Webアプリ開発</p>
+              <p>VSCode：Webアプリ開発</p>
             </div>
           </div>
         </div>
       </div>
 
       {/* 問い合わせ */}
-      <div className='h-321 flex flex-col items-center'>
-        <h1 className='text-[64px] text-center pt-75'>Contact me</h1>
-        <div className='h-190 w-178 pt-18 border-[3px] gap-8 border-[#D4C9BE] flex flex-col items-center'>
-          <div className='flex gap-16'>
+      <div className='h-auto flex flex-col items-center py-70'>
+        <h1 className='md:text-[64px] sm:text-[52px] text-[44px] text-center relative'>
+          Contact me
+          <span className='absolute bg-[var(--text-color)] md:w-110 sm:w-90 w-70 sm:h-[5px] h-[4px] bottom-0 left-1/2 transform -translate-x-1/2' />
+        </h1>
+        <div className='form:h-140 h-165 form:w-132 w-72 pt-5 border-[3px] gap-5 border-[#D4C9BE] flex flex-col items-center sm:mt-15 mt-7'>
+          <div className='flex flex-wrap justify-center form:gap-12 gap-8'>
             <div className='flex flex-col'>
-              <h2 className='text-[32px]'>お名前</h2>
-              <div className='h-18 w-70 border-[3px] border-[#D4C9BE]'></div>
+              <h2 className='form:text-[28px] text-2xl'>お名前</h2>
+              <div className='h-12 form:w-49 w-56 border-[3px] border-[#D4C9BE]'></div>
             </div>
             <div className='flex flex-col'>
-              <h2 className='text-[32px]'>アドレス</h2>
-              <div className='h-18 w-70 border-[3px] border-[#D4C9BE]'></div>
+              <h2 className='form:text-[28px] text-2xl'>アドレス</h2>
+              <div className='h-12 form:w-49 w-56 border-[3px] border-[#D4C9BE]'></div>
             </div>
           </div>
           <div className='flex flex-col'>
-            <h2 className='text-[32px]'>件名</h2>
-            <div className='h-18 w-156 border-[3px] border-[#D4C9BE]'></div>
+            <h2 className='form:text-[28px] text-2xl'>件名</h2>
+            <div className='h-12 form:w-110 w-56 border-[3px] border-[#D4C9BE]'></div>
           </div>
           <div className='flex flex-col'>
-            <h2 className='text-[32px]'>本文</h2>
-            <div className='h-50 w-156 border-[3px] border-[#D4C9BE]'></div>
+            <h2 className='form:text-[28px] text-2xl'>本文</h2>
+            <div className='h-50 form:w-110 w-56 border-[3px] border-[#D4C9BE]'></div>
           </div>
-          <button className='h-20 w-57 border-[3px] text-[30px] border-[#D4C9BE]'>送信する</button>
+          <button 
+            className='h-12 w-31 border-[3px] form:text-[28px] text-2xl border-[#D4C9BE]'
+            type='submit'
+          >
+            送信する
+          </button>
         </div>
       </div>
     </>
