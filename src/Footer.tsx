6445f5bd02194059691678631f27d5699d433a3c
Haruki140 <iharuki140@icloud.com>
2025-04-29 23:05:27 +0900

    [fix] コピーマークと年の追加

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,9 +1,18 @@
 import React, {FC} from 'react';
 
+function getCurrentYear() {
+    const date = new Date()
+    const year = date.getFullYear()
+    return year
+}
+
 const Footer: FC = () => {
+    const currenYear = getCurrentYear()
     return (
         <div className='h-12 bg-[var(--header-color)] text-center py-3'>
-            All Right Reserved
+            <span className='block max-w-screen-xl mx-auto text-lm'>
+                © {currenYear} Haruki140 All Right Reserved
+            </span>
         </div>
     )
 }
