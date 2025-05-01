107b23ac924a8cb3c1de1aeba18b8d181f7b1696
Haruki140 <iharuki140@icloud.com>
2025-05-01 20:31:55 +0900

    [fix] 軽微な修正

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
 import Router from './Routes'
-import Header from './Header'
-import Footer from './Footer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
