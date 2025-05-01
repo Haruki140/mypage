de74046af6e0ff8a53d9f78a14de423b2aabfe2c
Haruki140 <iharuki140@icloud.com>
2025-05-01 20:28:55 +0900

    [create] 型タイプ宣言をまとめた

---
+++ src/type.d.ts
@@ -0,0 +1,31 @@
+type Link = {
+    icon: string,
+    uri: string,
+    alt: number,
+}
+
+declare module 'type' {
+    interface Elements {
+        homeElement: HTMLElement | null,
+        aboutElement: HTMLElement | null,
+        workElement: HTMLElement | null,
+        skillElement: HTMLElement | null,
+        contactElement: HTMLElement | null,
+    }
+    interface pairElements {
+        title: string,
+        idx: HTMLElement | null,
+    }
+    interface Photo {
+        uri: string,
+        alt: number,
+    }    
+    interface WorkData {
+        title: string,
+        date: string,
+        detail: string,
+        skills: string,
+        links: Link[],
+        photos: Photo[],
+    }
+}
